9ad64602aefd16af5b2bd3a002454a980a1265cf
Thomas Hillyer <thomashillyer@users.noreply.github.com>
2023-08-28 21:12:29 -0700

    only run on client code, add event listener load instead of window.onload

---
+++ plugins/anim_extensions.client.ts
@@ -0,0 +1,30 @@
+export default defineNuxtPlugin(nuxtApp => {
+    /*
+    ** Only run on client-side and only in production mode
+    */
+    if (process.client) {
+
+        window.addEventListener('load', function () {
+            setTimeout(function () {
+                // Add the strikethrough animation
+                const textElement = document.getElementById('animText');
+                textElement?.classList.add('strikethrough-animation');
+
+                setTimeout(function () {
+                    // Fade out the text
+                    textElement?.classList.remove('strikethrough-animation');
+                    textElement?.classList.add('fade-out-animation');
+
+                    setTimeout(function () {
+                        // Replace the text and fade it in
+                        textElement!.textContent = "Hillzere";
+                        textElement?.classList.remove('fade-out-animation');
+                        textElement?.classList.add('fade-in-animation');
+                    }, 500);  // Duration of the fade out animation
+
+                }, 500);  // Duration of the strikethrough animation
+
+            }, 1000);  // 1 second delay before starting the animation
+        })
+    }
+});
